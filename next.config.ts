410b034147b488b1791eef1e94ab5d7ba8913c62
mutai-gilbert <mutaigilbert85@gmail.com>
2025-01-09 10:17:05 +0300

    Add static files for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Enable static export
+  // Add any other config options you may need here
 };
 
 export default nextConfig;
