82f173ae02ffcac54dfbe165818d0da81335f6b5
mutai-gilbert <mutaigilbert85@gmail.com>
2025-01-09 10:56:54 +0300

    Update next.config.ts for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/county-revenue-app',
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for static exports
+  },
 };
+
 export default nextConfig;
