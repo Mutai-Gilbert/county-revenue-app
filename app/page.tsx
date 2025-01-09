59b80f47ef994c0f35b05cc3e6aeeb59759fc3e0
Gilbert Mutai <mutaigilbert85@gmail.com>
2025-01-09 10:36:40 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div className="min-h-screen flex flex-col relative">
       {/* Full-screen background image */}
       <div className="absolute inset-0 w-full h-full">
-        <Img src={`/county-revenue-app/assets/kericho-landscape.jpg`} alt="Kericho Landscape" />
+        <Image src={`/county-revenue-app/assets/kericho-landscape.jpg`} alt="Kericho Landscape" />
       </div>
 
       {/* Content overlay */}
