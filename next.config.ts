8a426de9d394436fb132b4f4ab8de8ed42d41c8a
Gilbert Mutai <mutaigilbert85@gmail.com>
2025-01-09 11:13:49 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/county-revenue-app',
   images: {
     unoptimized: true, // Disable Next.js image optimization for static exports
   },
